ec6538f04fa4c9ac29f3fc13e3f64f6bfc6c46c9
Ruben Alvarez Reyes <rubenar1996@gmail.com>
2026-05-15 14:12:07 -0700

    working order

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import csv
 from dotenv import load_dotenv 
 import tarfile
 from mutagen.easyid3 import EasyID3
+from pathlib import Path
 
 
 class Main:
@@ -28,7 +29,15 @@ class Main:
             for file_name in filenames:
                 full_path = os.path.join(dirpath, file_name)
                 audio_metadata = self.get_metadata(full_path)
-                if len(audio_metadata) > 0:
+
+                all_empty = True
+                for m in audio_metadata:
+                    if m != '':
+                        all_empty = False
+
+                missing_tags = audio_metadata[0] == '' or audio_metadata[1] == ''
+
+                if not all_empty and not missing_tags:
                     csv_file_data.append(audio_metadata)
 
         with open(self.dest_csv_path, 'w') as csv_file:
@@ -41,16 +50,31 @@ class Main:
 
 
     def get_metadata(self, file_path: str) -> list:
-        metadata = []
+        metadata = ['' for _ in range(len(self.csv_headers))]
+        metadata[self.csv_headers.index('length')] = 0
 
         try:
             audio = EasyID3(file_path)
             keys = audio.keys()
+            for key in keys:
+                match key:
+                    case 'title':
+                        metadata[self.csv_headers.index('name')] = audio[key][0]
+                    case 'artist' | 'albumartist':
+                        metadata[self.csv_headers.index('artistName')] = audio[key][0]
+                    case 'album' | 'genre':
+                        metadata[self.csv_headers.index(key)] = audio[key][0]
+                    case 'length':
+                        metadata[self.csv_headers.index(key)] = int(audio[key][0])
+
+            file_path = os.path.join(*list(Path(file_path).parts[5:]))
+            metadata[self.csv_headers.index('audioPath')] = file_path
 
         except mutagen.MutagenError:
             pass
         finally:
             return metadata
+
 
 if __name__ == "__main__":
     load_dotenv()
